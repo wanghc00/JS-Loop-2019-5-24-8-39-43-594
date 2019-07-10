3cfe503ac89c139bdd02fd8ab7df9606082b4dbe
wanghc00 <306093226@qq.com>
2019-07-10 20:32:38 +0800

    2019-07-10

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        document.write(i + "是偶数");
+    }
+    if (i % 2 != 0) {
+        document.write(i + "是奇数");
+    }
+    document.write("<br />");
+}
